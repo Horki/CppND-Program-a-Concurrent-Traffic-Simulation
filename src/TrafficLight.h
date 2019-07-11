025ed6d51485fbd82e5937d5a6fcc65b01fbb5f8
Horki <ivanzvonimirhorvat@gmail.com>
2019-07-11 18:11:16 +0200

    init: indent fix

--- src/TrafficLight.h
+++ src/TrafficLight.h
@@ -16,12 +16,11 @@ class Vehicle;
 // Also, there should be an std::condition_variable as well as an std::mutex as private members. 
 
 template <class T>
-class MessageQueue
-{
+class MessageQueue {
 public:
 
 private:
-    
+
 };
 
 // FP.1 : Define a class „TrafficLight“ which is a child class of TrafficObject. 
@@ -30,24 +29,22 @@ private:
 // can be either „red“ or „green“. Also, add the private method „void cycleThroughPhases()“. 
 // Furthermore, there shall be the private member _currentPhase which can take „red“ or „green“ as its value. 
 
-class TrafficLight
-{
+class TrafficLight {
 public:
-    // constructor / desctructor
+  // constructor / desctructor
 
-    // getters / setters
+  // getters / setters
 
-    // typical behaviour methods
+  // typical behaviour methods
 
 private:
-    // typical behaviour methods
+  // typical behaviour methods
 
-    // FP.4b : create a private member of type MessageQueue for messages of type TrafficLightPhase 
-    // and use it within the infinite loop to push each new TrafficLightPhase into it by calling 
-    // send in conjunction with move semantics.
-
-    std::condition_variable _condition;
-    std::mutex _mutex;
+  // FP.4b : create a private member of type MessageQueue for messages of type TrafficLightPhase
+  // and use it within the infinite loop to push each new TrafficLightPhase into it by calling
+  // send in conjunction with move semantics.
+  std::condition_variable _condition;
+  std::mutex _mutex;
 };
 
 #endif
--- src/TrafficObject.cpp
+++ src/TrafficObject.cpp
@@ -1,6 +1,7 @@
 #include <algorithm>
 #include <iostream>
 #include <chrono>
+#include <algorithm>
 #include "TrafficObject.h"
 
 // init static variable
@@ -8,28 +9,24 @@ int TrafficObject::_idCnt = 0;
 
 std::mutex TrafficObject::_mtx;
 
-void TrafficObject::setPosition(double x, double y)
-{
-    _posX = x;
-    _posY = y;
+void TrafficObject::setPosition(double x, double y) {
+  _posX = x;
+  _posY = y;
 }
 
-void TrafficObject::getPosition(double &x, double &y)
-{
-    x = _posX;
-    y = _posY;
+void TrafficObject::getPosition(double &x, double &y) {
+  x = _posX;
+  y = _posY;
 }
 
-TrafficObject::TrafficObject()
-{
-    _type = ObjectType::noObject;
-    _id = _idCnt++;
+TrafficObject::TrafficObject() {
+  _type = ObjectType::noObject;
+  _id = _idCnt++;
 }
 
-TrafficObject::~TrafficObject()
-{
-    // set up thread barrier before this object is destroyed
-    std::for_each(threads.begin(), threads.end(), [](std::thread &t) {
-        t.join();
-    });
+TrafficObject::~TrafficObject() {
+  // set up thread barrier before this object is destroyed
+  std::for_each(threads.begin(), threads.end(), [](std::thread &t) {
+    t.join();
+  });
 }
